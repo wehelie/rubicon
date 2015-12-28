f5e3da9671fc5ac20a4b9af36dffd5aa801a6a89
wehelie <wehelie@users.noreply.github.com>
2015-12-28 14:39:10 -0800

    Update and rename rubicon.py to rubicon.js

---
+++ rubicon.js
@@ -0,0 +1,22 @@
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
+
+fetch('/users')
+  .then(checkStatus)
+  .then(parseJSON)
+  .then(function(data) {
+    console.log('request succeeded with JSON response', data)
+  }).catch(function(error) {
+    console.log('request failed', error)
+  })
